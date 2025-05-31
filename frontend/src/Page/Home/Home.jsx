16bd31de957c91a46b63e73042efabd13b90cf58
Tejas khope <tejaskhope600@gmail.com>
2025-05-31 18:03:49 +0530

    aboutproject completed

--- frontend/src/Page/Home/Home.jsx
+++ frontend/src/Page/Home/Home.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import Hero from '../../component/Hero/Hero'
-import AboutProject from '../../component/AboutProject/AboutProject'
+import React from "react";
+import Hero from "../../component/Hero/Hero";
+import AboutProject from "../../component/AboutProject/AboutProject";
 
-         
 function Home(login, setLogin) {
   return (
-   <>
-   <Hero setLogin={setLogin}/>
-   <AboutProject />
-   </>
-  )
+    <>
+      <Hero setLogin={setLogin} />
+      <AboutProject />
+    </>
+  );
 }
 
-export default Home
+export default Home;
